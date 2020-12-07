322dae9cde8dfaa8dd0c83ff9cc4a5a8d03190af
James Bakker <james.orion.bakker@gmail.com>
2020-12-07 10:30:37 -0800

    first commit, added diviser

---
+++ divisers.js
@@ -0,0 +1,25 @@
+function getDivisers(a, b){
+  //Find starting number, init variables
+	let gcdArr = [];
+	let theGCD;
+	if (a > b){theGCD = a;} else {theGCD = b;}
+	// Start Loop
+  //Test to see if diviser works for both numbers
+	do{
+    let qA = a / theGCD;
+    let qB = b / theGCD;
+
+    if(Number.isInteger(qA) && Number.isInteger(qB)){
+    	//if the diviser works push to array
+      gcdArr.push(theGCD)
+    }
+    //increment diviser down by 1
+    theGCD = theGCD - 1;
+    //keep looping until diviser reaches 1
+	} while(theGCD > 0);
+  // return array
+  return gcdArr;
+}
+
+let result = getDivisers(4093012,68602824)
+console.log(result);
